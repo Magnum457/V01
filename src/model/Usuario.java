6b08f8c8147c5c858e48e6351ae91edea1ba90e9
Magno Felipe <mfts201116@gmail.com>
2019-03-11 07:44:10 -0300

    Criação do objeto usuario e dos seus atributos

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Usuario {
-
+	// atributos
+		private String login;
+		private String nome;
+		private String email;
+		
+	// getters and setters
+		public String getLogin() {
+			return login;
+		}
+		public void setLogin(String login) {
+			this.login = login;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
 }
